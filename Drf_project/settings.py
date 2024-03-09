26c028971b19ce729e73e619c1699e756c05f1bf
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-03-09 12:12:16 +0330

    doing pagination

--- Drf_project/settings.py
+++ Drf_project/settings.py
@@ -132,6 +132,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
 
 SIMPLE_JWT = {
