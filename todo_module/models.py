1420360a183304ee02e6dcfb53605f095431ea87
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-03-09 12:16:47 +0330

    work on nested serializers

--- todo_module/models.py
+++ todo_module/models.py
@@ -1,14 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 # Create your models here.
 
-
 class Todo(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=300)
     description = models.TextField()
     is_done = models.BooleanField(default=False)
-    priority = models.IntegerField(default=0)
+    priority = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+
+    def __str__(self):
+        return f'{self.title} / Is Done: {self.is_done}'
 
     class Meta:
-        db_table = 'todo'
+        db_table = 'todos'
--- todo_module/serializers.py
+++ todo_module/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Todo
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class TodoSerializer(serializers.ModelSerializer):
+
+    def validate_priority(self, priority):
+        if priority <= 0 or priority > 20:
+            raise serializers.ValidationError("Priority is not ok !")
+        return priority
+
     class Meta:
         model = Todo
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    todos = TodoSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
